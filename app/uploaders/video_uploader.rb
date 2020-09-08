57124651144e195518dbd130c698449fa7aa692c
天間仁 <jin@tenmahitoshinoMacBook-Air.local>
2020-09-08 18:25:06 +0900

    'master'

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -6,8 +6,6 @@ class VideoUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   
   if Rails.env.production?
-    storage :fog
-  else
     storage :file #=> ローカルストレージ
   end
   # Override the directory where uploaded files will be stored.
